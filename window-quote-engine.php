d4cf2c953a97c070551d2c76c445557cf284a9ec
Ash Stratton <ash.stratton@supadu.com>
2023-05-12 19:54:54 +0100

    working php window

--- window-quote-engine.php
+++ window-quote-engine.php
@@ -8,9 +8,12 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
 ?>
 
     <div class="container">
+
         <div class="calculator-container">
+
             <div id="step1" class="step">
                 <h2>Window Style</h2>
+
                 <div class="grid">
                     <?php foreach($windowStyles as $windowStyle): ?>
                         <?php $windowStyleName = $windowStyle['window_style']["window_style_name"]; ?>
@@ -21,34 +24,44 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
                         </div>
                     <?php endforeach; ?>
                 </div>
+
                 <div class="validation-message"></div>
+
                 <div class="button-container">
-                <button id="show-quote-btn" style="display:none;">Show My Quote So Far</button>
+                    <button id="view-quote-summary-btn">View My Quote Summary</button>
                     <button class="next">Next</button>
                 </div>
+
             </div>
 
 
             <div id="step2" class="step hidden">
+
                 <h2>Window Dimensions</h2>
+
                 <div class="dimensions-container">
                     <label for="width">Width (mm):</label>
                     <input type="number" id="width" name="width" min="0" required>
                     <label for="height">Height (mm):</label>
                     <input type="number" id="height" name="height" min="0" required>
                 </div>
+
                 <div class="validation-message"></div>
+
                 <div class="button-container">
-                    <button id="show-quote-btn" style="display:none;">Show My Quote So Far</button>
                     <button class="prev">Previous</button>
                     <button class="next">Next</button>
                 </div>
+
             </div>
 
 
             <div id="step3" class="step hidden">
+
                 <h2>External Frame Colour</h2>
+
                 <div class="grid">
+
                     <?php foreach($windowExternalFrameColors as $windowExternalFrameColor): ?>
                         <?php $windowExternalFrameColorName = $windowExternalFrameColor['window_external_frame_color']["window_external_frame_color_name"]; ?>
                         <?php $windowExternalFrameColorImageUrl = $windowExternalFrameColor['window_external_frame_color']["window_external_frame_color_swatch"]['url']; ?>
@@ -57,19 +70,25 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
                             <p><?php echo $windowExternalFrameColorName; ?></p>
                         </div>
                     <?php endforeach; ?>
+
                 </div>
+
                 <div class="validation-message"></div>
+
                 <div class="button-container">
-                    <button id="show-quote-btn" style="display:none;">Show My Quote So Far</button>
                     <button class="prev">Previous</button>
                     <button class="next">Next</button>
                 </div>
+
             </div>
 
 
             <div id="step4" class="step hidden">
+
                 <h2>Internal Frame Colour</h2>
+
                 <div class="grid">
+
                     <?php foreach($windowInternalFrameColors as $windowInternalFrameColor): ?>
                         <?php $windowInternalFrameColorName = $windowInternalFrameColor['window_internal_frame_color']["window_internal_frame_color_name"]; ?>
                         <?php $windowInternalFrameColorImageUrl = $windowInternalFrameColor['window_internal_frame_color']["window_internal_frame_color_swatch"]['url']; ?>
@@ -78,18 +97,22 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
                             <p><?php echo $windowInternalFrameColorName; ?></p>
                         </div>
                     <?php endforeach; ?>
+
                 </div>
+
                 <div class="validation-message"></div>
+
                 <div class="button-container">
-                    <button id="show-quote-btn" style="display:none;">Show My Quote So Far</button>
                     <button class="prev">Previous</button>
                     <button class="next">Next</button>
                 </div>
+
             </div>
 
             <div id="step4_5" class="step hidden">
                 <h2>Handle Colour</h2>
                 <div class="grid">
+
                     <?php foreach($windowHandleColors as $windowHandleColor): ?>
                         <?php $windowHandleColorName = $windowHandleColor['window_handle_color']["window_handle_color_name"]; ?>
                         <?php $windowHandleColorImageUrl = $windowHandleColor['window_handle_color']["window_handle_color_swatch"]['url']; ?>
@@ -98,11 +121,13 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
                             <p><?php echo $windowHandleColorName; ?></p>
                         </div>
                     <?php endforeach; ?>
+
+
                     <!-- Add more handle colors as needed -->
                 </div>
                 <div class="validation-message"></div>
                 <div class="button-container">
-                <button id="show-quote-btn" style="display:none;">Show My Quote So Far</button>
+
                     <button class="prev">Previous</button>
                     <button class="next">Next</button>
                 </div>
@@ -112,6 +137,7 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
             <div id="step5" class="step hidden">
                 <h2>Glazing Style</h2>
                 <div class="grid">
+
                 <?php foreach($windowGlazingStyles as $windowGlazingStyle): ?>
                         <?php $windowGlazingStyleName = $windowGlazingStyle['window_glazing_style']["window_glazing_style_name"]; ?>
                         <?php $windowGlazingStyleImageUrl = $windowGlazingStyle['window_glazing_style']["window_glazing_style_image"]['url']; ?>
@@ -120,11 +146,11 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
                             <p><?php echo $windowGlazingStyleName; ?></p>
                         </div>
                     <?php endforeach; ?>
+                    
                     <!-- Add more glazing styles as needed -->
                 </div>
                 <div class="validation-message"></div>
                 <div class="button-container">
-                    <button id="show-quote-btn" style="display:none;">Show My Quote So Far</button>
                     <button class="prev">Previous</button>
                     <button id="next-step5" class="next">Next</button>
                 </div>
@@ -132,42 +158,49 @@ $windowGlazingStyles = get_field('window_glazing_styles', 'options');
 
 
             <div id="step6" class="step hidden">
-                <div id="summary"><h3>Summary</h3></div>
+
+                <div id="summary">
+                    <h3>Summary</h3>
+
+                </div>
+
                 <form id="contact-form">
-                    <div class="form-inline-container">
-                        <div class="form-field">
-                            <label for="name">Name:</label>
-                            <input type="text" id="name" name="name" class="form-input-half" placeholder="Name" required>
-                        </div>
-                        <div class="form-field">
-                            <label label for="email">Email:</label>
-                            <input type="email" id="email" name="email" class="form-input-half" placeholder="Email" required>
-                        </div>
+                   
+                <div class="form-inline-container">
+                    <div class="form-field">
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="name" class="form-input-half" placeholder="Name" required>
                     </div>
-                    <div class="form-inline-container">
-                        <div class="form-field">
-                            <label for="phone">Phone:</label>
-                            <input type="tel" id="phone" name="phone" class="form-input-whole" placeholder="Phone" required>
-                        </div>
-                        <div class="form-field">
-                            <label for="postcode">Postcode:</label>
-                            <input type="text" id="postcode" name="postcode" class="form-input-whole" placeholder="Postcode" required>
-                        </div>
+                    <div class="form-field">
+                        <label label for="email">Email:</label>
+                        <input type="email" id="email" name="email" class="form-input-half" placeholder="Email" required>
                     </div>
+                </div>
+                <div class="form-inline-container">
+                    <div class="form-field">
+                        <label for="phone">Phone:</label>
+                        <input type="tel" id="phone" name="phone" class="form-input-whole" placeholder="Phone" required>
+                    </div>
+                    <div class="form-field">
+                        <label for="postcode">Postcode:</label>
+                        <input type="text" id="postcode" name="postcode" class="form-input-whole" placeholder="Postcode" required>
+                    </div>
+                </div>
                     <div class="button-container">
-                        <button id="configure-another-window">Add Another Window</button>
+                        <button id="configure-another-window">Configure Another Window</button>
                         <button class="submit-quote" type="submit">Submit</button>
                     </div>
+                    
                 </form>
             </div>
         </div>
 
         <div class="image-side" style="background:url('<?php echo $windowQuoteEngineImage; ?>')">
             <div class="quote-engine-selector-container">
-                <!-- <div class="quote-engine-tile">Window Quote</div>
+                <div class="quote-engine-tile">Window Quote</div>
                 <div class="quote-engine-tile">Door Quote</div>
                 <div class="quote-engine-tile">Conservatory Quote</div>
-                <div class="quote-engine-tile">Replacement Conservatory Roof Quote</div> -->
+                <div class="quote-engine-tile">Replacement Conservatory Roof Quote</div>
             </div>
         </div>
     </div>
